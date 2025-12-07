d737486a5e6305ea51c0c4425a565a8870313363
Kyle Johnson <kyjo1355@colorado.edu>
2025-12-07 15:14:48 -0700

    fixed weapon chest

--- src/main/java/game/chest/WeaponChest.java
+++ src/main/java/game/chest/WeaponChest.java
@@ -7,26 +7,15 @@ import game.artifacts.WeaponFactory;
 import java.util.Random;
 
 public class WeaponChest {
-    private static final int MINIMUM_VALUE_FOR_RANDOM_INT_RANGE = 1;
-    private static final int NUM_WEAPON_TYPES = 3;
 
     private final WeaponFactory factory;
-
-
 
     public WeaponChest(WeaponFactory factory) {
         this.factory = factory;
     }
 
-    public  Weapon createWeapon() {
-        int randomWeaponType = MINIMUM_VALUE_FOR_RANDOM_INT_RANGE + new Random().nextInt(NUM_WEAPON_TYPES);
-        if (randomWeaponType == MINIMUM_VALUE_FOR_RANDOM_INT_RANGE){
-            return factory.createBasicWeapon();
-        }
-        else if (randomWeaponType == NUM_WEAPON_TYPES){
-            return factory.createGreatWeapon();
-        }
-        return factory.createGoodWeapon();
+    public Weapon createWeapon() {
+        return factory.createBasicWeapon();
     }
 
 }
